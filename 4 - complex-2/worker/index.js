65b156a4f2a3469136c53415c3478b3d93248ddd
Jordan Bailey <bailey8@buffalo.edu>
2021-12-29 01:53:40 -0500

    first

---
+++ 4 - complex-2/worker/index.js
@@ -0,0 +1,27 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+
+  // If the redis client ever loses connection to the redis server it should try every 1 second to reconnect to it
+  retry_strategy: () => 1000,
+});
+
+// Create a duplicate of the redis client. Sub stands for subscription
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+// Anytime we get a new message, ruin this callback function right here
+sub.on('message', (channel, message) => {
+
+  // redis is a key value store. The key is the message (index), the value is the fib associate with
+  // This line sets the value associatied with the key (the message)
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
---
+++ 4 - complex/server/index.js
@@ -0,0 +1,80 @@
+const keys = require("./keys");
+
+// Express App Setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Cors allows us to make requests from 1 domain that the react
+// app will be running on to a diff domain that the express API will be rnning on
+app.use(cors());
+
+// Turns the incoming post requests into a json obj
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.error(err));
+});
+
+// Redis Client Setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
+
+  res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+
+  // The worker will eventually replace this with the calculated value
+  redisClient.hset("values", index, "Nothing yet!");
+
+  // This will be sent to the wroker process and wake it up and say its time to pull a new value out of redis
+  // and claculate the value for it
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log("Listening");
+});
